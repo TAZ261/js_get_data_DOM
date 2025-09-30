0a254b673a59288e307bdfb03fcf169f2363fc98
Roman <hpp1ovh@gmail.com>
2025-09-30 21:22:49 +0300

    complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const populations = document.querySelectorAll('.population');
+
+const values = Array.from(populations).map((el) => {
+  return Number(el.textContent.replace(/,/g, ''));
+});
+
+const total = values.reduce((sum, num) => sum + num, 0);
+const average = total / values.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-US');
